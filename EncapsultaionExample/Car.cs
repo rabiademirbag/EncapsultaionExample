81a3876142eaeed83b83ee955dce760d37ecffbb
rabiademirbag <rddemirbagg@gmail.com>
2025-05-21 17:06:51 +0300

    Proje dosyası ekle.

---
+++ EncapsultaionExample/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsultaionExample
+{
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private string color;
+        private int numberOfDoor;
+
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set {  model = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        } 
+        public int NumberOfDoor
+        {
+            get
+            {
+                return numberOfDoor;
+            }
+            set{ 
+            if(value!=2 && value != 4)
+                {
+                    Console.WriteLine("2 veya 4 dışında kapı sayısı girilemez.");
+                    numberOfDoor = -1;
+                }
+            else
+                {
+                    numberOfDoor = value;
+                }
+            }
+        }
+        public void CarInfo()
+        {
+            Console.WriteLine("Car brand: "+brand+","+"Car model: "+model+","+"Color: "+color+","+"Number of door: "+numberOfDoor);
+        }
+    }
+}
---
+++ EncapsultaionExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using EncapsultaionExample;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Brand = "Land Rover";
+        car.Model = "Range Rover Velar";
+        car.Color = "Batumi Gold";
+        car.NumberOfDoor = 1;
+
+        car.CarInfo();
+    } 
+}
